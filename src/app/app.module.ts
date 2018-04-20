75917c6de6539d29388e7e4f2d7464ba0a0b958a
Hamza Ouchrif <maradona_hamza@hotmail.com>
2018-04-20 10:27:46 -0400

    Première route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AngularFontAwesomeModule } from 'angular-font-awesome';
 import { BienvenueComponent } from './bienvenue/bienvenue.component';
+import { RouterModule } from '@angular/router/src/router_module';
 
 @NgModule({
   declarations: [
@@ -15,7 +16,10 @@ import { BienvenueComponent } from './bienvenue/bienvenue.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    AngularFontAwesomeModule
+    AngularFontAwesomeModule,
+    RouterModule.forRoot([
+      {path:'bienvenue', component:BienvenueComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
